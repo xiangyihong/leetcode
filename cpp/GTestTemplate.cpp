1b36be9f85788b93118647da368b02eb1af26568
Xiang, Yihong <xiangyihong@gmail.com>
2016-01-19 15:06:02 +0800

    Add a template file for gtest

---
+++ cpp/GTestTemplate.cpp
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h"
+#include <vector>
+#include <string>
+
+
+struct ParamsAndExpectedResult
+{
+
+};
+
+TEST(TemplateTestFile, NullTest)
+{
+	std::vector<ParamsAndExpectedResult> test_cases =
+	{
+	};
+
+	for (int i = 0; i < test_cases.size(); ++i)
+	{
+
+	}
+}
