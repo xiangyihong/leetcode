e700eb834f7c375fd8d2b94272c9665f8006e945
Xiang Yihong <xiangyihong@gmail.com>
2016-01-25 00:00:00 +0800

    add comments

--- cpp/137_SingleNumberII.cpp
+++ cpp/137_SingleNumberII.cpp
@@ -10,6 +10,9 @@ public:
 
 		for (int i = 0; i < 32; ++i)
 		{
+			// count the frequency of ith bit
+			// if the count of ith bit is a multiple of 3
+			// single number's ith bit must be 0
 			for (int j = 0; j < nums.size(); ++j)
 			{
 				if (nums[j] & (1 << i))
